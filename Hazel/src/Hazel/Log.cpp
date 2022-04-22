44bbd2cce65a93145fccb34e3eb09a65dfa70bda
Shan Jia <73159723+Js11252001@users.noreply.github.com>
2022-04-22 21:57:02 +0800

    initialize the static variable

--- Hazel/src/Hazel/Log.cpp
+++ Hazel/src/Hazel/Log.cpp
@@ -4,8 +4,8 @@
 
 namespace Hazel {
 
-	std::shared_ptr<spdlog::logger> Log::s_CoreLogger;
-	std::shared_ptr<spdlog::logger> Log::s_ClientLogger;
+	std::shared_ptr<spdlog::logger> Log::s_CoreLogger = nullptr;
+	std::shared_ptr<spdlog::logger> Log::s_ClientLogger = nullptr;
 
 	void Log::Init()
 	{
